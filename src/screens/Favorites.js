730689b96e288d1f82f6489a61079b3f97979d5e
Igor <ponjoru@users.noreply.github.com>
2018-10-17 13:51:27 +0300

    Add google map support

--- src/screens/Favorites.js
+++ src/screens/Favorites.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import styled from 'styled-components/native';
-
+import MapView from 'react-native-maps';
+import {StyleSheet, View} from 'react-native';
 
 const ContainerView = styled.View`
   flex: 1;
@@ -14,13 +15,25 @@ const TitleText = styled.Text`
 `;
 
 class FavoritesScreen extends Component {
-  render() {
-    return (
-      <ContainerView>
-        <TitleText>Favorites</TitleText>
-      </ContainerView>
-    );
-  }
+    render() {
+        return (
+            <View style={{flex:1, justifyContent: 'flex-end'}}>
+                <MapView  style={styles.map}
+                          region={{
+                              latitude: 60.0074,
+                              longitude: 30.3729,
+                              latitudeDelta: 0.005,
+                              longitudeDelta: 0.005,
+                          }}
+                />
+            </View>
+        );
+    }
 }
 
+const styles = StyleSheet.create({
+    map:{
+        flex: 1,
+    }
+});
 export default FavoritesScreen;
