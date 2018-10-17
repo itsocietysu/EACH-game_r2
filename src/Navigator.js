f13bee035693d4fd75ba0df00bb279aea1f826c1
Igor <ponjoru@users.noreply.github.com>
2018-10-17 16:46:33 +0300

    Added museums canvas support

--- src/Navigator.js
+++ src/Navigator.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import { Platform } from 'react-native';
 import {
-  TabNavigator,
-  StackNavigator,
-  DrawerNavigator,
-  HeaderBackButton,
+    TabNavigator,
+    StackNavigator,
+    DrawerNavigator,
+    HeaderBackButton,
 } from 'react-navigation';
 import { FontAwesome, Ionicons } from '@expo/vector-icons';
 
 import WelcomeScreen from './screens/Welcome';
 import HomeScreen from './containers/HomePage/FeedPage';
-import ProfileScreen from './screens/Profile';
+
 import FavoritesScreen from './screens/Favorites';
+import MuseumsScreen from './containers/MuseumPage/MuseumPage';
 import SettingsScreen from './screens/Settings';
 
 import { HamburgerIcon, SettingsIcon, BackIcon } from './components/icons';
@@ -21,145 +22,149 @@ import { colors } from './utils/constants';
 import LoginScreen from "./screens/Login";
 import RegistrationScreen from "./screens/Registration"
 
+
 const AppMainTab = TabNavigator({
-  Home: {
-    screen: HomeScreen,
-    navigationOptions: ({ navigation }) => ({
-      drawerLabel: 'Sweet home',
-      drawerIcon: ({ tintColor }) => (
-        <FontAwesome name="home" size={23} color={tintColor} />
-      ),
-      tabBarLabel: 'Home',
-      tabBarIcon: ({ tintColor }) => (
-        <FontAwesome name="home" size={23} color={tintColor} />
-      ),
-      headerStyle: {
-        backgroundColor: colors.SOFT,
-      },
-      headerTitle: 'Feed',
-      headerTitleStyle: {
-        color: colors.WHITE,
-      },
-      headerLeft: <HamburgerIcon onPress={() => navigation.navigate('DrawerOpen')} />,
-    })
-  },
-  Favorites: {
-    screen: FavoritesScreen,
-    navigationOptions: ({ navigation }) => ({
-      drawerLabel: 'Favorites',
-      drawerIcon: ({ tintColor }) => (
-        <FontAwesome name="heartbeat" size={23} color={tintColor} />
-      ),
-      tabBarLabel: 'Favorites',
-      tabBarIcon: ({ tintColor }) => (
-        <FontAwesome name="heartbeat" size={23} color={tintColor} />
-      ),
-      headerStyle: {
-        backgroundColor: colors.PINK_100,
-      },
-      headerTitle: 'Favorites',
-      headerTitleStyle: {
-        color: colors.WHITE,
-      },
-      headerLeft: <HamburgerIcon onPress={() => navigation.navigate('DrawerOpen')} />,
-    })
-  },
-  Profile: {
-    screen: ProfileScreen,
-    navigationOptions: ({ navigation }) => ({
-      drawerLabel: 'Profile',
-      drawerIcon: ({ tintColor }) => (
-        <FontAwesome name="user-circle" size={23} color={tintColor} />
-      ),
-      tabBarLabel: 'Profile',
-      tabBarIcon: ({ tintColor }) => (
-        <FontAwesome name="user-circle" size={23} color={tintColor} />
-      ),
-      headerStyle: {
-        backgroundColor: colors.PINK_100,
-      },
-      headerTitle: 'Profile',
-      headerTitleStyle: {
-        color: colors.WHITE,
-      },
-      headerLeft: <HamburgerIcon onPress={() => navigation.navigate('DrawerOpen')} />,
-      headerRight: <SettingsIcon onPress={() => navigation.navigate('Settings')} />,
-    })
-  },
-}, {
-  tabBarOptions: {
-    activeTintColor: colors.WHITE,
-    inactiveTintColor: colors.PINK_50,
-    inactiveBackgroundColor: colors.PINK_100,
-    activeBackgroundColor: colors.PINK_100,
-    showIcon: true,
-    showLabel: Platform.OS === 'ios',
-    indicatorStyle: {
-      backgroundColor: colors.PINK_300,
+        Home: {
+            screen: HomeScreen,
+            navigationOptions: ({ navigation }) => ({
+                drawerLabel: 'Sweet home',
+                drawerIcon: ({ tintColor }) => (
+                    <FontAwesome name="home" size={23} color={tintColor} />
+                ),
+                tabBarLabel: 'Home',
+                tabBarIcon: ({ tintColor }) => (
+                    <FontAwesome name="home" size={23} color={tintColor} />
+                ),
+                headerStyle: {
+                    backgroundColor: colors.SOFT,
+                },
+                headerTitle: 'Feed',
+                headerTitleStyle: {
+                    color: colors.WHITE,
+                },
+                headerLeft: <HamburgerIcon onPress={() => navigation.navigate('DrawerOpen')} />,
+            })
+        },
+        Favorites: {
+            screen: FavoritesScreen,
+            navigationOptions: ({ navigation }) => ({
+                drawerLabel: 'Maps',
+                drawerIcon: ({ tintColor }) => (
+                    <FontAwesome name="heartbeat" size={23} color={tintColor} />
+                ),
+                tabBarLabel: 'Maps',
+                tabBarIcon: ({ tintColor }) => (
+                    <FontAwesome name="heartbeat" size={23} color={tintColor} />
+                ),
+                headerStyle: {
+                    backgroundColor: colors.PINK_100,
+                },
+                headerTitle: 'Maps',
+                headerTitleStyle: {
+                    color: colors.WHITE,
+                },
+                headerLeft: <HamburgerIcon onPress={() => navigation.navigate('DrawerOpen')} />,
+            })
+        },
+        Profile: {
+            screen: MuseumsScreen,
+            navigationOptions: ({ navigation }) => ({
+                drawerLabel: 'Profile',
+                drawerIcon: ({ tintColor }) => (
+                    <FontAwesome name="user-circle" size={23} color={tintColor} />
+                ),
+                tabBarLabel: 'Profile',
+                tabBarIcon: ({ tintColor }) => (
+                    <FontAwesome name="user-circle" size={23} color={tintColor} />
+                ),
+                headerStyle: {
+                    backgroundColor: colors.PINK_100,
+                },
+                headerTitle: 'Profile',
+                headerTitleStyle: {
+                    color: colors.WHITE,
+                },
+                headerLeft: <HamburgerIcon onPress={() => navigation.navigate('DrawerOpen')} />,
+                headerRight: <SettingsIcon onPress={() => navigation.navigate('Settings')} />,
+            })
+        },
     },
-    style: {
-      backgroundColor: colors.PINK_100,
-    },
-    upperCaseLabel: false,
-  },
-  tabBarPosition: 'bottom',
-  swipeEnabled: false,
-  animationEnabled: false,
-});
+    {
+        tabBarOptions: {
+            activeTintColor: colors.WHITE,
+            inactiveTintColor: colors.PINK_50,
+            inactiveBackgroundColor: colors.PINK_100,
+            activeBackgroundColor: colors.PINK_100,
+            showIcon: true,
+            showLabel: Platform.OS === 'ios',
+            indicatorStyle: {
+                backgroundColor: colors.PINK_300,
+            },
+            style: {
+                backgroundColor: colors.PINK_100,
+            },
+            upperCaseLabel: false,
+        },
+        tabBarPosition: 'bottom',
+        swipeEnabled: false,
+        animationEnabled: false,
+    }
+);
 
 const AppMainStack = StackNavigator({
     Home: { screen: AppMainTab },
-  Settings: { screen: SettingsScreen },
+    Settings: { screen: SettingsScreen },
 }, {
-  cardStyle: {
-    backgroundColor: colors.PINK_50,
-  },
-  mode: 'modal',
+    cardStyle: {
+        backgroundColor: colors.PINK_50,
+    },
+    mode: 'modal',
 });
 
 const AppDrawer = DrawerNavigator({
-  Home: {
-    screen: AppMainStack,
-  },
-  Settings: {
-    screen: SettingsScreen,
-    navigationOptions: ({ navigation }) => ({
-      drawerLabel: 'Settings',
-      drawerIcon: ({ tintColor }) => (
-        <Ionicons name="md-settings" size={23} color={tintColor} />
-      ),
-      headerStyle: {
-        backgroundColor: colors.PINK_100,
-      },
-      headerTitle: 'Settings',
-      headerTitleStyle: {
-        color: colors.WHITE,
-      },
-      headerLeft: <BackIcon onPress={() => navigation.goBack()} />,
-		}),
-  },
+    Home: { screen: AppMainStack, },
+    Settings: {
+        screen: SettingsScreen,
+        navigationOptions: ({ navigation }) => ({
+            drawerLabel: 'Settings',
+            drawerIcon: ({ tintColor }) => (
+                <Ionicons name="md-settings" size={23} color={tintColor} />
+            ),
+            headerStyle: {
+                backgroundColor: colors.PINK_100,
+            },
+            headerTitle: 'Settings',
+            headerTitleStyle: {
+                color: colors.WHITE,
+            },
+            headerLeft: <BackIcon onPress={() => navigation.goBack()} />,
+        }),
+    },
 }, {
-  contentComponent: props =>
-    (<CustomDrawerContent
-      {...props}
-		/>),
-  contentOptions: {
-    activeBackgroundColor: colors.PINK_100,
-    activeTintColor: colors.WHITE,
-		inactiveTintColor: colors.PINK_200,
-  },
+    contentComponent: props =>
+        (<CustomDrawerContent
+            {...props}
+        />),
+    contentOptions: {
+        activeBackgroundColor: colors.PINK_100,
+        activeTintColor: colors.WHITE,
+        inactiveTintColor: colors.PINK_200,
+    },
 });
 
-const Navigator = TabNavigator({
-  Welcome: { screen: WelcomeScreen },
-  Login: { screen: LoginScreen },
-  Registration: {screen: RegistrationScreen},
-  Main: { screen: AppDrawer },
-}, {
-  navigationOptions: {
-    tabBarVisible: false,
-  },
-  swipeEnabled: false,
-});
+const Navigator = TabNavigator(
+    {
+        Welcome: { screen: WelcomeScreen },
+        Login: { screen: LoginScreen },
+        Registration: {screen: RegistrationScreen},
+        Main: { screen: AppDrawer },
+    },
+    {
+        navigationOptions: {
+            tabBarVisible: false,
+        },
+        swipeEnabled: false,
+    }
+);
 
 export default Navigator;
---
+++ src/containers/MuseumPage/saga.js
@@ -0,0 +1,39 @@
+import { call, put, takeLatest } from 'redux-saga/effects';
+import { LOAD_MUSEUMS} from './constants';
+import { museumsLoaded, museumsLoadingError } from './actions';
+import request from './../../utils/request';
+
+/**
+ * Feeds data load handler
+ */
+export function* loadMuseums() {
+    const requestURL = `http://each.itsociety.su:4201/each/museum/all`;
+    try {
+        const museums = yield call(request, requestURL);
+        let data = false;
+        if (museums.length) {
+            data = museums.map(item => ({
+                eid: item.eid,
+                name: item.name,
+                desc: item.desc,
+                location: item.location,
+                game: item.game,
+                image: `${
+                    item.image[0] ? `http://${item.image[0].url}` : '/Photo.png'
+                    }`,
+
+                // priority: `${item.priority[0] ? item.priority[0] : 0}`,
+            }));
+        }
+        yield put(museumsLoaded(data));
+    } catch (err) {
+        yield put(museumsLoadingError(err));
+    }
+}
+
+/**
+ * Root saga manages watcher lifecycle
+ */
+export default function* loadMuseumsData() {
+    yield takeLatest(LOAD_MUSEUMS, loadMuseums);
+}
