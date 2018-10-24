5ebafda7e35d808d55487d380a8d6a2e8969937e
LanskovNV <leinssniel@gmail.com>
2018-10-24 15:16:58 +0300

    added onPress option to museums markers

--- src/screens/MapPage.js
+++ src/screens/MapPage.js
@@ -2,7 +2,7 @@
 import React, { Component } from 'react';
 import PropTypes from "prop-types";
 import MapView, { Marker } from 'react-native-maps';
-import { Dimensions, ImageBackground, StyleSheet, TouchableOpacity, View } from "react-native";
+import { StyleSheet,  View } from "react-native";
 import { createStructuredSelector } from 'reselect';
 import { connect } from 'react-redux';
 import { compose } from 'redux';
@@ -10,8 +10,9 @@ import { loadMuseums } from "../containers/MuseumPage/actions";
 import { makeSelectData, makeSelectError, makeSelectLoading } from "../containers/MuseumPage/selectors";
 import injectSaga from "../utils/injectSaga";
 import saga from "../containers/MuseumPage/saga";
-import { DescriptionText, TextContainer, TittleText } from "../containers/styles";
-import { MuseumItemScreen } from "../containers/MuseumPage/MuseumItem"
+// import { DescriptionText, TextContainer, TittleText } from "../containers/styles";
+// import { MuseumItemScreen } from "../containers/MuseumPage/MuseumItem"
+
 
 const LATITUDE = 60.0074;
 const LONGITUDE = 30.3729;
@@ -20,18 +21,14 @@ const LONGITUDE_DELTA = LATITUDE_DELTA;
 let id = 0;
 
 class MapsScreen extends Component {
-
   componentDidMount() {
     if (!this.props.data) this.props.init();
-  }
-
-  onMarkerPress() {
-
   }
 
   render() {
     const { data } = this.props;
     let markers = [];
+
     if (data) {
       data.map(museum => {
         const arr = museum.location.map(location => (
@@ -44,8 +41,9 @@ class MapsScreen extends Component {
             image = {require('./../../assets/icons/map_icon_128.png')}
             title={location.name}
             description={museum.name.RU}
-            onPress={()=>{this.props.navigation.navigate('MuseumPage', {data: museum})}}
-          />));
+            onPress={()=>{this.props.navigation.navigate('MuseumPage', {data: museum})}} // this.props.navigation.navigate('MuseumPage', {data: museum})}
+          />
+        ));
         markers = markers.concat(arr);
       })
     }
@@ -90,6 +88,11 @@ const mapStateToProps = createStructuredSelector({
 });
 
 const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
     map:{
         flex: 1,
     }
