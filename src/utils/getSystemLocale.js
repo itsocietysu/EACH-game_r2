742d0b65b7f880bad1322636bebc9f6ac416b8b6
Igor <ponjoru@users.noreply.github.com>
2018-11-01 14:20:57 +0300

    Added initial locale initialized by device system locale
    Android support added
    iOS support added

--- src/utils/getSystemLocale.js
+++ src/utils/getSystemLocale.js
@@ -4,12 +4,10 @@ import {Platform} from 'react-native';
 export default async function getSystemLocaleAsync(){
     const locale =  await Util.getCurrentLocaleAsync();
     if (Platform.OS === 'android') {
-        alert(locale.substring(0, 2));
         return new Promise(resolve => resolve(locale.substring(0,2)));
     }
     else if (Platform.OS === 'ios'){
-        alert(locale);
-        return new Promise(resolve=>resolve(locale));
+        return new Promise(resolve => resolve(locale.substring(0,2)));
     }
     return 'en';
 }
