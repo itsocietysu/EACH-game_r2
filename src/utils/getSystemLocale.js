f3fa82d198152ff28a42ba3dc00c26994dc1c76c
Igor <ponjoru@users.noreply.github.com>
2018-11-01 14:27:19 +0300

    Improved perfomance

--- src/utils/getSystemLocale.js
+++ src/utils/getSystemLocale.js
@@ -3,10 +3,7 @@ import {Platform} from 'react-native';
 
 export default async function getSystemLocaleAsync(){
     const locale =  await Util.getCurrentLocaleAsync();
-    if (Platform.OS === 'android') {
-        return new Promise(resolve => resolve(locale.substring(0,2)));
-    }
-    else if (Platform.OS === 'ios'){
+    if (Platform.OS === 'android' || Platform.OS === 'ios') {
         return new Promise(resolve => resolve(locale.substring(0,2)));
     }
     return 'en';
