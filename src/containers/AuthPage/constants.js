d6a6523d3d50ae4fb39bc3466d7b3a51536bd35a
Nikita Lanskov <leinssniel@gmail.com>
2018-11-22 18:50:38 +0300

    Added google auth support(getting auth code only)

--- src/containers/AuthPage/constants.js
+++ src/containers/AuthPage/constants.js
@@ -1,3 +1,4 @@
 export const VK_URL = 'https://oauth.vk.com/authorize';
 export const EACH_URL = 'http://each.itsociety.su:5000/oauth2/authorize?response_type=code&client_id=Gu2SCEBUwQV3TSlNIu8uMzvKRMYuGP5ePh044jGErO6O9RR0&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fauth&scope=email&state=TW9uIE9jdCAwOCAyMDE4IDE4OjU2OjAwIEdNVCswMzAwICjQnNC%2B0YHQutCy0LAsINGB0YLQsNC90LTQsNGA0YLQvdC%2B0LUg0LLRgNC10LzRjyk%3D';
 export const GOOGLE_URL = 'https://accounts.google.com/o/oauth2/auth';
+export const GOOGLE_REDIRECT_URL = 'https://auth.expo.io/auth'; // 'http://each.itsociety.su:10000/auth';
