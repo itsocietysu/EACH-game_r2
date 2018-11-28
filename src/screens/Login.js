c1bb3c8f81e57ffc0d6328ae91b5dbbea8eb2ae8
Nikita Lanskov <leinssniel@gmail.com>
2018-11-28 23:47:13 +0300

    Added getting auth code funcs to vk and each and google

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -7,15 +7,12 @@ import {
 } from 'react-native'
 import { AuthSession } from 'expo';
 import EachIcon from "../components/icons/EachIcon";
+import GoogleIcon from "../components/icons/GoogleIcon";
+import VkontakteIcon from "../components/icons/VkontakteIcon";
 
-const eachClientId = '190923403189-srp0gleu6imvtph8gcauf03uhb66q65h.apps.googleusercontent.com';
-const eachAuthDomain = 'http://each.itsociety.su:5000/oauth2/authorize?';
-
-function toQueryString(params) {
-  return Object.entries(params)
-    .map(([key, value]) => `${encodeURIComponent(key)}=${encodeURIComponent(value)}`)
-    .join('&');
-}
+const googleClientId = '190923403189-srp0gleu6imvtph8gcauf03uhb66q65h.apps.googleusercontent.com';
+const eachClientId = 'Gu2SCEBUwQV3TSlNIu8uMzvKRMYuGP5ePh044jGErO6O9RR0';
+const vkontakteClientId = '6682398';
 
 class LoginScreen extends Component {
   constructor(props) {
@@ -25,23 +22,61 @@ class LoginScreen extends Component {
       password: '',
       redirectCode: '',
     };
-    this.signInWithEachAsync = this.signInWithEachAsync.bind(this);
+    this.signInWithGoogleAsync = this.signInWithGoogleAsync.bind(this);
   }
 
-
-  async signInWithEachAsync() {
+  async signInWithGoogleAsync() {
     try {
       const redirectUrl = AuthSession.getRedirectUrl();
-      console.log(`Redirect URL (add this to Auth0): ${redirectUrl}`);
       const result = await AuthSession.startAsync({
         authUrl:
           `https://accounts.google.com/o/oauth2/auth?` +
-          `&client_id=${encodeURIComponent(eachClientId)}` +
+          `&client_id=${encodeURIComponent(googleClientId)}` +
           `&redirect_uri=${encodeURIComponent(redirectUrl)}` +
           `&response_type=code` +
           `&scope=${encodeURIComponent(['profile','email'].join(' '))}`,
       });
-      console.log(result);
+      if (result.type === 'success')
+        return result.params.code;
+      return { cancelled: true };
+    } catch (e) {
+      return { error: true };
+    }
+  }
+
+  async signInWithEachAsync() {
+    try {
+      const redirectUrl = AuthSession.getRedirectUrl();
+      const result = await AuthSession.startAsync({
+        authUrl:
+          `http://each.itsociety.su:5000/oauth2/authorize?` +
+          `&client_id=${encodeURIComponent(eachClientId)}` +
+          `&redirect_uri=${encodeURIComponent(redirectUrl)}` +
+          `&response_type=code` +
+          `&scope=${encodeURIComponent(['email'].join(' '))}`,
+      });
+      if (result.type === 'success')
+        return result.params.code;
+      return { cancelled: true };
+    } catch (e) {
+      return { error: true };
+    }
+  }
+
+  async signInWithVkontakteAsync() {
+    try {
+      const redirectUrl = AuthSession.getRedirectUrl();
+      console.log(redirectUrl);
+      const result = await AuthSession.startAsync({
+        authUrl:
+          `https://oauth.vk.com/authorize?` +
+          `&client_id=${encodeURIComponent(vkontakteClientId)}` +
+          `v=${encodeURIComponent("5.85")}` +
+          `display=mobile` +
+          `&redirect_uri=${encodeURIComponent(redirectUrl)}` +
+          `&response_type=code` +
+          `&scope=${encodeURIComponent(['offline','email'].join(' '))}`,
+      });
       if (result.type === 'success')
         return result.params.code;
       return { cancelled: true };
@@ -58,8 +93,12 @@ class LoginScreen extends Component {
           style={styles.logoContainer}>
           <Text>Sign in</Text>
           <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'center' }}>
+            <GoogleIcon size={65}
+                        onPress={() => {this.signInWithGoogleAsync().then(code => this.setState({redirectCode: code}));}}/>
             <EachIcon size={65}
                         onPress={() => {this.signInWithEachAsync().then(code => this.setState({redirectCode: code}));}}/>
+            <VkontakteIcon size={65}
+                        onPress={() => {this.signInWithVkontakteAsync().then(code => this.setState({redirectCode: code}));}}/>
           </View>
           <Text>{`DON'T HAVE AN ACCOUNT YET? ${this.state.redirectCode}`}</Text>
           <Text style={{ color: '#0000ff' }}>{`Sign up`}</Text>
