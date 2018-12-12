3b6f5b5f5fdc8f7616e53f5d00c2477681756f91
Igor <ponjoru@users.noreply.github.com>
2018-12-12 17:22:04 +0300

    TextQuest updated

--- src/components/Picker.js
+++ src/components/Picker.js
@@ -1,13 +1,34 @@
 import React, {Component} from 'react'
 import {View, Text, TouchableOpacity, Dimensions} from 'react-native';
-import {AnswerContainer, AnswerText} from "../containers/styles";
-import {colors} from "../utils/constants";
+import {colors, fonts} from "../utils/constants";
+import styled from "styled-components/native";
+import getFont from "../utils/getFont";
 
+export const AnswerText = styled.Text`
+    fontSize: 20px
+    paddingLeft: 10px
+    textAlign: center
+    fontFamily: ${props => props.font}
+    color: ${props => props.color}
+`;
+
+export const AnswerContainer = styled.View`
+    flex: 1
+    flexDirection: row
+    backgroundColor: ${props => props.color}
+    borderWidth: 2
+    borderColor: ${colors.SECOND.light}
+    height: 50px
+    width: ${props => props.width*7/8}
+    justifyContent: center
+    alignItems: center
+`;
 export default class PickerItem extends Component {
 
     render(){
         const text = this.props.text;
         const theme = this.props.theme;
+        const fontLoaded = this.props.fontLoaded;
         const state = this.props.state;
         const handler = this.props.handler;
         const width = Dimensions.get('window').width;
@@ -16,8 +37,8 @@ export default class PickerItem extends Component {
         return(
             <TouchableOpacity onPress={()=>handler(text.index)}>
                 <View style={{flex:1, justifyContent: 'center', alignItems: 'center', paddingBottom: 10, backgroundColor: colors.BASE[theme]}}>
-                    <AnswerContainer color={bgColor} width={width*7/10}>
-                        <AnswerText color={txtColor}>{text.item}</AnswerText>
+                    <AnswerContainer color={bgColor} width={width*0.95}>
+                        <AnswerText color={txtColor} font={getFont(fontLoaded, fonts.EACH)}>{text.item}</AnswerText>
                     </AnswerContainer>
                 </View>
 
--- src/containers/Question/TextQuestion.js
+++ src/containers/Question/TextQuestion.js
@@ -1,22 +1,49 @@
 import React, {Component} from 'react';
-import {View, Alert, Text, FlatList, TouchableOpacity, ImageBackground, Dimensions, ScrollView} from 'react-native';
+import {View, Alert, Image, Text, FlatList, TouchableOpacity, ImageBackground, Dimensions, ScrollView} from 'react-native';
 import {withNavigation} from 'react-navigation';
 
 import {TEXT_QUESTION, AR_PAINT_QUESTION, LOCATION_QUESTION} from "./constants";
 import CustomList from "../../components/CustomList";
 import PickerItem from "../../components/Picker";
 import styled from "styled-components/native";
-import {SpamHello, CentroidFigure, ButtonText, StyledButton} from "../styles";
+import {SpamHello, CentroidFigure, StyledButton} from "../styles";
 import messages from "../../Messages";
 import {FormattedWrapper, FormattedMessage} from "react-native-globalize";
-import {colors} from "../../utils/constants";
+import {colors, fonts} from "../../utils/constants";
+import {createStructuredSelector} from "reselect";
+import {makeSelectError, makeSelectLoading, makeSelectResult} from "../../components/ValidateImage/selectors";
+import {makeSelectFonts} from "../../components/Fonts/selectors";
+import connect from "react-redux/es/connect/connect";
+import {mapDispatchToProps} from "./ARQuestion";
+import {makeSelectLanguage} from "../../components/Locales/selectors";
+import {makeSelectTheme} from "../../components/Theme/selectors";
+import {compose} from "redux";
+import getFont from "../../utils/getFont";
+import ArrowButton from "../../components/ArrowButton";
 
 
 const QuestionText = styled.Text`
     color: ${props => props.color}
+    fontFamily: ${props => props.font}
     fontSize: 20px
     fontWeight: bold
     textAlign: center
+    paddingTop: 10
+`;
+
+const DescText = styled.Text`
+    alignSelf: center
+    color: ${props => props.color}
+    fontFamily: ${props => props.font}
+    fontSize: 20
+    paddingTop: 20
+`;
+
+const ButtonText = styled.Text`
+    alignSelf: center
+    color: ${props => props.color}
+    fontFamily: ${props => props.font}
+    fontSize: 20
 `;
 
 class TextQuestion extends Component{
@@ -55,30 +82,45 @@ class TextQuestion extends Component{
     render(){
         const step = this.props.data;
         const theme = this.props.theme;
-        const width = Dimensions.get('window').width;
+        const locale = this.props.locale.toUpperCase();
+        const fontLoaded = this.props.font;
+        const {width, height} = Dimensions.get('window');
         return(
-            <FormattedWrapper locale={this.props.language} messages={messages}>
+            <FormattedWrapper locale={this.props.locale} messages={messages}>
                 <View style={{flex: 1, backgroundColor: colors.BASE[theme]}}>
                     <ScrollView style={{flex: 1}}>
-                        <QuestionText color={colors.TEXT[theme]}>{step.question}</QuestionText>
-                        <View style={{paddingBottom: 10}}>
-                            <ImageBackground source={{uri: step.avatar.uri}}
-                                     style={{width: width, height: width/2}}/>
+                        <View style={{flexDirection: 'row', paddingTop: 5, paddingLeft:5, paddingRight: 5}}>
+                            <Image source={{uri: step.avatar.uri}}
+                                   style={{width: width*0.45, height: width*0.45}}/>
+                            <View style={{flex: 1, paddingLeft: 5}}>
+                                <DescText color={colors.MAIN} font={getFont(fontLoaded, fonts.MURRAY)}>
+                                    <FormattedMessage message={'ChoiceTaskDesc'}/>
+                                </DescText>
+                            </View>
+
                         </View>
+                        <QuestionText color={colors.TEXT[theme]} font={getFont(fontLoaded, fonts.MURRAY)}>
+                            {step.question}
+                        </QuestionText>
+
                         <FlatList
                             data={step.choices}
-                            renderItem={(item)=><PickerItem text={item} state={this.state} theme={theme} handler={this._changeSelection}/>}
+                            renderItem={(item)=><PickerItem text={item} state={this.state} theme={theme} fontLoaded={fontLoaded} handler={this._changeSelection}/>}
                             keyExtractor={(item) => step.choices.indexOf(item)}
                             extraData={[theme, this.state]}
                             scrollEnabled={false}
                         />
-                        <TouchableOpacity onPress={() => this._validateResult(this)}>
-                            <CentroidFigure>
-                                <StyledButton color={colors.THIRD[theme]}>
-                                    <ButtonText color={colors.BUTTON_TEXT[theme]}><FormattedMessage message={'Validate'}/></ButtonText>
-                                </StyledButton>
-                            </CentroidFigure>
-                        </TouchableOpacity>
+                        <View style={{alignItems: 'flex-end', paddingRight: 30}}>
+                            <ArrowButton
+                                onPress={() => this._validateResult(this)}
+                                bgColor={colors.BASE[theme]}
+                                borderColor={colors.MAIN}
+                                width={width*0.55}
+                                height={height*0.075}
+                            >
+                                <ButtonText color={colors.TEXT[theme]} font={getFont(fontLoaded, fonts.EACH)}><FormattedMessage message={'Validate'}/>-></ButtonText>
+                            </ArrowButton>
+                        </View>
                         <View style={{height: 20}}/>
                     </ScrollView>
                 </View>
@@ -87,4 +129,18 @@ class TextQuestion extends Component{
     }
 }
 
-export default withNavigation(TextQuestion);
+const mapStateToProps = createStructuredSelector({
+    locale: makeSelectLanguage(),
+    theme: makeSelectTheme(),
+    font: makeSelectFonts(),
+});
+
+const withConnect = connect(
+    mapStateToProps,
+    {},
+);
+
+export default compose(
+    withConnect,
+    withNavigation
+)(TextQuestion);
